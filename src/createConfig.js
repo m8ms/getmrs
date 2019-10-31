c0bfe3fd997f09b09061b97b30dba168c5ff9655
pmartyniak <piotr.martyniak@gmail.com>
2019-10-31 12:59:33 +0100

    chore: remove editorconfig in favor of prettier
    add husky + pre-commit format
    reformat code with prettier defaults

--- src/createConfig.js
+++ src/createConfig.js
@@ -1,48 +1,52 @@
-const fs = require('fs')
-const api = require('./api.js')
-const inquirer = require('inquirer')
-const { highlight } = require('./draw')
+const fs = require("fs");
+const api = require("./api.js");
+const inquirer = require("inquirer");
+const { highlight } = require("./draw");
 
 module.exports = async function createConfig(configPath, initialConfig = {}) {
-	const config = await inquirer.prompt([
-		{
-			type: 'input',
-			name: 'server',
-			message: 'Gitlab server address',
-			default: initialConfig.server || 'https://gitlab.com',
-			filter: str => str.trim(),
-		},
-		{
-			type: 'input',
-			name: 'token',
-			message: 'Gitlab token',
-			default: initialConfig.token,
-			filter: str => str.trim(),
-		},
-	])
+  const config = await inquirer.prompt([
+    {
+      type: "input",
+      name: "server",
+      message: "Gitlab server address",
+      default: initialConfig.server || "https://gitlab.com",
+      filter: str => str.trim()
+    },
+    {
+      type: "input",
+      name: "token",
+      message: "Gitlab token",
+      default: initialConfig.token,
+      filter: str => str.trim()
+    }
+  ]);
 
-	api.setup(config)
+  api.setup(config);
 
-	const { data } = await api.getUserProjects()
-	const choices = data.map(({ name }) => name)
+  const { data } = await api.getUserProjects();
+  const choices = data.map(({ name }) => name);
 
-	const { projects } = await inquirer.prompt({
-		type: 'checkbox',
-		name: 'projects',
-		message: 'Projects you want to review (can be overridden with: "getmrs -p comma,separated,names; no selection == "all")',
-		choices
-	})
+  const { projects } = await inquirer.prompt({
+    type: "checkbox",
+    name: "projects",
+    message:
+      'Projects you want to review (can be overridden with: "getmrs -p comma,separated,names; no selection == "all")',
+    choices
+  });
 
-	fs.writeFile(
-		configPath,
-		JSON.stringify({
-			...config,
-			...projects.length && { projects }
-		}),
-		(err) => {
-			if (err) throw err;
-			console.log(
-				`\nConfig saved to:\n${highlight(configPath)}\nRun script again to start using.\n`
-			);
-		})
-}
+  fs.writeFile(
+    configPath,
+    JSON.stringify({
+      ...config,
+      ...(projects.length && { projects })
+    }),
+    err => {
+      if (err) throw err;
+      console.log(
+        `\nConfig saved to:\n${highlight(
+          configPath
+        )}\nRun script again to start using.\n`
+      );
+    }
+  );
+};
--- src/getmrs.js
+++ src/getmrs.js
@@ -1,49 +1,56 @@
-const fs = require('fs')
-const os = require('os')
-const path = require('path')
-const parseArgs = require('minimist')
-const api = require('./api')
-const createConfig = require('./createConfig')
-const { filterProjects, buildTable } = require('./utils')
-const { slightlyDarker, draw } = require('./draw')
+const fs = require("fs");
+const os = require("os");
+const path = require("path");
+const parseArgs = require("minimist");
+const api = require("./api");
+const createConfig = require("./createConfig");
+const { filterProjects, buildTable } = require("./utils");
+const { slightlyDarker, draw } = require("./draw");
 
-const args = parseArgs(process.argv.slice(2))
+const args = parseArgs(process.argv.slice(2));
 
-const FORCE_CONFIG = args.c
-const CONFIG_FILE_PATH = path.join(os.homedir(), '.getmrs_config')
+const FORCE_CONFIG = args.c;
+const CONFIG_FILE_PATH = path.join(os.homedir(), ".getmrs_config");
 
 try {
-	const configRaw = fs.readFileSync(CONFIG_FILE_PATH)
-	const config = JSON.parse(configRaw)
+  const configRaw = fs.readFileSync(CONFIG_FILE_PATH);
+  const config = JSON.parse(configRaw);
 
-	if (FORCE_CONFIG) {
-		console.log('Overwriting config at: ' + CONFIG_FILE_PATH + '\n')
-		createConfig(CONFIG_FILE_PATH, config)
-	} else {
-		run(config)
-	}
-
+  if (FORCE_CONFIG) {
+    console.log("Overwriting config at: " + CONFIG_FILE_PATH + "\n");
+    createConfig(CONFIG_FILE_PATH, config);
+  } else {
+    run(config);
+  }
 } catch (err) {
-
-	if (err.code === 'ENOENT') {
-		console.log('First, we must generate a config file at: ' + CONFIG_FILE_PATH + '\n')
-		console.log(slightlyDarker('You can overwrite this config later by running "getmrs -c"\n'))
-		createConfig(CONFIG_FILE_PATH)
-	} else {
-		console.error(err)
-	}
+  if (err.code === "ENOENT") {
+    console.log(
+      "First, we must generate a config file at: " + CONFIG_FILE_PATH + "\n"
+    );
+    console.log(
+      slightlyDarker(
+        'You can overwrite this config later by running "getmrs -c"\n'
+      )
+    );
+    createConfig(CONFIG_FILE_PATH);
+  } else {
+    console.error(err);
+  }
 }
 
 async function run(config) {
-	api.setup(config)
-	const { data: user } = await api.getUser()
+  api.setup(config);
+  const { data: user } = await api.getUser();
 
-	const { data: projects } = await api.getUserProjects()
-	const selectedProjectNamesOrIds = args.p || process.env.GITLAB_PROJECTS || config.projects
-	const filteredProjects = selectedProjectNamesOrIds ? filterProjects(projects, selectedProjectNamesOrIds) : projects
+  const { data: projects } = await api.getUserProjects();
+  const selectedProjectNamesOrIds =
+    args.p || process.env.GITLAB_PROJECTS || config.projects;
+  const filteredProjects = selectedProjectNamesOrIds
+    ? filterProjects(projects, selectedProjectNamesOrIds)
+    : projects;
 
-	const mergeRequests = await api.getMergeRequests(filteredProjects)
-	const participants = await api.getParticipants(mergeRequests)
-	const data = buildTable({ participants, mergeRequests, projects, user })
-	draw(data)
+  const mergeRequests = await api.getMergeRequests(filteredProjects);
+  const participants = await api.getParticipants(mergeRequests);
+  const data = buildTable({ participants, mergeRequests, projects, user });
+  draw(data);
 }
